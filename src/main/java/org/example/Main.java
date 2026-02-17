3fe59d69bbbdd93f48efc4552869545e017b6c31
yağmur gamze çimen <yagmurgamzecimen@gmail.com>
2026-02-17 08:49:46 +0300

    feat: day1 task completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,37 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) return false;
+        return isBarking && (clock < 8 || clock >= 20);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return (firstAge >= 13 && firstAge<= 19) ||
+               (secondAge >= 13 && secondAge<= 19) ||
+               (thirdAge >= 13 && thirdAge<= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     if(isSummer){
+         return temp >= 25 && temp<=45;
+     }else{
+         return temp >= 25 && temp <= 35;
+     }
     }
 
     public static double area(double width, double height) {
-       
+       if(width < 0 && height < 0){
+           return -1;
+       }else{
+           return width*height;
+       }
     }
 
     public static double area(double radius) {
-       
+       if(radius < 0){
+           return -1;
+       }else{
+           return radius*radius*Math.PI;
+       }
     }
 }
